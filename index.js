359ab4a02f86e0258cdab281bc388ae80a074693
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-07 08:23:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let num1;
+let num2;
+num1 = 2;
+num2 = 31;
+multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+let mod;
+let num3 =24;
+let num4 = 5;
+mod = num3 % num4;
+
+let max = Math.max(0, 5 ,20, 15);
